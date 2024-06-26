fixed loss calculation and variable num of bboxes

data/VHS/vhs_loader.py
@@ -19,13 +19,14 @@ def image_open_bw(img_path):
         return img.convert('L')
 
 class CoordinateDataset(Dataset):
-    def __init__(self, root_dir, im_sz, output_res, augment=False, num_workers=32, only10=False, testing=False):
+    def __init__(self, root_dir, csv, im_sz, output_res, augment=False, num_workers=32, only10=False, testing=False):
         self.root_dir = root_dir
         self.im_sz = im_sz
         self.output_res = output_res
         self.augment = augment
         self.testing = testing
-        csv_file = os.path.join(root_dir, 'Data.csv')
+        csv_path = f"Data_{csv}.csv"
+        csv_file = os.path.join(root_dir, csv_path)
         self.data_frame = pd.read_csv(csv_file, header=0).head(10) if only10 else pd.read_csv(csv_file, header=0)
 
         image_paths = [os.path.join(self.root_dir, img_name) for img_name in self.data_frame.iloc[:, 0]]
@@ -38,29 +39,28 @@ class CoordinateDataset(Dataset):
     def __getitem__(self, idx):
         image = self.images[idx]
         points = self.data_frame.iloc[idx, 1:].values.astype('float').reshape(-1, 2)
-
+        
+        # Filter out NaN values
+        valid_points = points[~np.isnan(points).any(axis=1)]
+        
         if self.augment:
-            image, points = custom_transform(image, points)
+            image, valid_points = custom_transform(image, valid_points)
 
         image_tensor = transforms.Compose([
-            # transforms.Resize((self.im_sz, self.im_sz)),
-            # transforms.Grayscale(num_output_channels=3),
             transforms.ToTensor(),
         ])(image)
 
-        #image_tensor = F.pad(image_tensor, (0, 1, 0, 1), value=0)
+        heatmaps = self.generate_heatmaps(valid_points, self.output_res)
 
-        heatmaps = self.generate_heatmaps(points, self.output_res)
-
-        if self.testing: return image_tensor, points
+        if self.testing: return image_tensor, valid_points
 
         return image_tensor, heatmaps
 
     def generate_heatmaps(self, points, output_res):
         num_keypoints = len(points)
         heatmaps = np.zeros((num_keypoints, output_res, output_res), dtype=np.float32)
-        for i in range(num_keypoints):
-            x, y = int(points[i, 0] * output_res), int(points[i, 1] * output_res)
+        for i, point in enumerate(points):
+            x, y = int(point[0] * output_res), int(point[1] * output_res)
             if 0 <= x < output_res and 0 <= y < output_res:
                 heatmaps[i, y, x] = 1
                 heatmaps[i] = ndimage.gaussian_filter(heatmaps[i], sigma=1)

models/posenet.py
@@ -61,50 +61,22 @@ class PoseNet(nn.Module):
                 x = x + self.merge_preds[i](preds) + self.merge_features[i](feature)
         return torch.stack(combined_hm_preds, 1)
 
-    # def heatmapLoss.forward(self, pred, gt):
-    #     l = ((pred - gt)**2)
-    #     l = l.mean(dim=3).mean(dim=2).mean(dim=1)
-    #     return l
     def calc_loss(self, combined_hm_preds, heatmaps):
-        # combined_hm_preds shape: [bs, nstack, oup_dim, output_res, output_res]
-        # heatmaps shape: [bs, oup_dim, output_res, output_res]
-
         def heatmapLoss(pred, gt):
-            # pred shape: [bs, oup_dim, output_res, output_res]
-            # gt shape:  [bs, oup_dim, output_res, output_res]
-
-
-            #weights = torch.tensor([1.1, 1.1, 1, 1, 1, 1]).view(1, -1, 1, 1)
-            # Ensure weights are on the same device as your predictions (e.g., CPU or CUDA)
-            #weights = weights.to(pred.device)
-            # Calculate weighted squared differences
-            weighted_squared_diff = (pred - gt)**2# * weights
-            # Compute the mean, taking into account the weighted differences
-            total_loss = weighted_squared_diff.mean(dim=3).mean(dim=2).mean(dim=1)
-
-            return {'total_loss': total_loss}#, 'basic_loss': basic_loss, 'focused_loss': focused_loss}
+            mask = (gt.sum(dim=(2, 3)) > 0).float().unsqueeze(2).unsqueeze(3)
+            weighted_squared_diff = ((pred - gt) ** 2) * mask
+            total_loss = weighted_squared_diff.sum() / (mask.sum() + 1e-6)
+            return {'total_loss': total_loss}
 
         combined_total_loss = []
-        combined_basic_loss = []
-        combined_focused_loss = []
 
         for i in range(self.nstack):
-            # for a single batch
-            #loss_outputs = self.heatmapLoss(combined_hm_preds[0][:,i], heatmaps)
-            loss_outputs = heatmapLoss(combined_hm_preds[0][:,i], heatmaps)
+            loss_outputs = heatmapLoss(combined_hm_preds[:, i], heatmaps)
             combined_total_loss.append(loss_outputs["total_loss"])
-            # combined_basic_loss.append(loss_outputs["basic_loss"])
-            # combined_focused_loss.append(loss_outputs["focused_loss"])
 
-        # Stack the total, basic, and focused losses separately
-        combined_total_loss = torch.stack(combined_total_loss, dim=1)
-        # combined_basic_loss = torch.stack(combined_basic_loss, dim=1)
-        # combined_focused_loss = torch.stack(combined_focused_loss, dim=0)
+        combined_total_loss = torch.stack(combined_total_loss, dim=0)
 
-        # Return a dictionary containing the combined losses
         return {
-            # "combined_basic_loss": combined_basic_loss,
-            # "combined_focused_loss": combined_focused_loss,
             "combined_total_loss": combined_total_loss
         }
 